0075e433c64cbcd5ab5a0f5f2389902386b14ad2
Jens Kristian Hoel <jens.kristian.hoel@seaonics.com>
2020-01-02 16:11:55 +0100

    chg: added base functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,67 @@ import React from 'react';
 
 import Sidebar from './recursive-sidebar';
 
+// Default Menu Items
+const MenuItems = [
+  { name: 'home', label: 'Home' },
+  {
+    name: 'billing',
+    label: 'Billing',
+    items: [
+      { name: 'statements', label: 'Statements' },
+      { name: 'reports', label: 'Reports' }
+    ]
+  },
+  {
+    name: 'settings',
+    label: 'Settings',
+    items: [
+      { name: 'profile', label: 'Profile' },
+      { name: 'insurance', label: 'Insurance' },
+      {
+        name: 'notifications',
+        label: 'Notifications',
+        items: [
+          { name: 'email', label: 'Email' },
+          {
+            name: 'desktop',
+            label: 'Desktop',
+            items: [
+              { name: 'schedule', label: 'Schedule' },
+              { name: 'frequency', label: 'Frequency' }
+            ]
+          },
+          { name: 'sms', label: 'SMS' }
+        ]
+      }
+    ]
+  }
+];
+
+// Helper function to generate random strings
+function randomStr(len, arr) {
+  var ans = '';
+  for (var i = len; i > 0; i--) {
+    ans += arr[Math.floor(Math.random() * arr.length)];
+  }
+  return ans;
+}
+
+// App Component
 export default function App() {
+  const [items, setItems] = React.useState(MenuItems);
+
+  const addMenuItem = () => {
+    const str = randomStr(8, '12345abcde');
+    setItems((prev) => [...prev, { name: str, label: str }]);
+  };
+
   return (
     <div>
-      <Sidebar />
+      <div style={{ position: 'absolute', right: 0 }}>
+        <button onClick={addMenuItem}>Add Menu Item</button>
+      </div>
+      <Sidebar items={items} />
     </div>
   );
 }
--- src/components/recursive-sidebar/index.jsx
+++ src/components/recursive-sidebar/index.jsx
@@ -1,26 +1,74 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
 
-const List = styled.ul`
-  list-style: none;
-
-  font-family: "Lucida Grande", "Lucida Sans Unicode", "Lucida Sans", Geneva, Arial, sans-serif;
+// Styled Components
+const Container = styled.div`
+  max-width: 10vw;
+  border: 1px solid rgba(0, 0, 0, 0.1);
 `;
 
-const ListItem = styled.li`
+const List = styled.div`
+  list-style: none;
+
+  font-family: 'Lucida Grande', 'Lucida Sans Unicode', 'Lucida Sans', Geneva,
+    Arial, sans-serif;
+`;
+
+const ListItem = styled.div`
   cursor: pointer;
 
   &:hover {
-    text-decoration: underline;
+    background-color: gray;
   }
 `;
 
-export default function Sidebar() {
+const ListItemLabel = styled.span``;
+
+// SidebarItem Component
+function SidebarItem({ label, items, depthStep = 10, depth = 1, ...rest }) {
   return (
-    <List>
-      <ListItem>Home</ListItem>
-      <ListItem>Billing</ListItem>
-      <ListItem>Settings</ListItem>
-    </List>
-  )
+    <React.Fragment>
+      <ListItem {...rest}>
+        <ListItemLabel style={{ paddingLeft: depth * depthStep, fontSize: `${1 - (depth / 100 )}rem`, fontWeight: 700 - (100 * depth) }}>{label}</ListItemLabel>
+      </ListItem>
+      {Array.isArray(items) ? (
+        <List>
+          {items.map((subItem) => (
+            <SidebarItem
+              key={subItem.name}
+              depth={depth + 1}
+              depthStep={depthStep}
+              {...subItem}
+            />
+          ))}
+        </List>
+      ) : null}
+    </React.Fragment>
+  );
+}
+
+// Sidebar Component
+export default function Sidebar({ items, depthStep, depth }) {
+  const [listItems, setListItems] = React.useState([]);
+
+  React.useEffect(() => {
+    const newItems = items.map((sidebarItem, index) => (
+      <SidebarItem
+        key={`${sidebarItem.name}${index}`}
+        depthStep={depthStep}
+        depth={depth}
+        {...sidebarItem}
+      />
+    ));
+
+    console.log(newItems);
+
+    setListItems(newItems);
+  }, [items, depthStep, depth]);
+
+  return (
+    <Container>
+      <List>{listItems}</List>
+    </Container>
+  );
 }
